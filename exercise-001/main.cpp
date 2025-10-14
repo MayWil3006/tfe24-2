d220f34b0bf88e7eff5c8883d537d2f91acb02e6
MayWil3006 <willmann.maya-it24@it.dhbw-ravensburg.de>
2025-10-14 13:46:25 +0200

    feat: add exercise number two

--- exercise-001/main.cpp
+++ exercise-001/main.cpp
@@ -3,6 +3,16 @@
 
 #include "config.h"
 
+void changeValueByParameter(int value) {
+    value = 99;
+    fmt::print("In der Funktion changeValueByParameter ist value = {}\n", value);
+}
+
+void changeValueByReference(int& ref) {
+    ref = 77;
+    fmt::print("In der Funktion changeValueByReference ist ref = {}\n", ref);
+}
+
 auto main(int argc, char **argv) -> int
 {
 
@@ -14,6 +24,32 @@ auto main(int argc, char **argv) -> int
     fmt::print("Hello, {}!\n", tfe24::PROJECT_NAME);
 
     /* INSERT YOUR CODE HERE */
+
+    int x = 10;
+    int y = 5;
+    int& r = x;
+    fmt::print("Alte Werte: \n");
+    fmt::print("Der Wert von x ist: {}\n", x);
+    fmt::print("Der Wert von r ist: {}\n", r);
+
+    r = 42;
+
+    fmt::print("Neue Werte: \n");
+    fmt::print("Der neue Wert von x ist: {}\n", x);
+    fmt::print("Der neue Wert von r ist: {}\n\n", r);
+
+
+    changeValueByParameter(x);
+    fmt::print("Auswirkungen auf x durch changeValueByParameter: x = {}\n", x);
+    changeValueByReference(x);
+    fmt::print("Auswirkungen auf x durch changeValueByReference: x = {}\n\n", x);
+
+    fmt::print("Ursprünglicher Wert von y = {}\n", y);
+    changeValueByParameter(y);
+    fmt::print("Auswirkungen auf y durch changeValueByParameter: y = {}\n", y);
+    fmt::print("Ursprünglicher Wert von y = {}\n", y);
+    changeValueByReference(y);
+    fmt::print("Nach der Funktion changeValueByReference: y = {}\n", y);
 
     return 0; /* exit gracefully*/
 }
