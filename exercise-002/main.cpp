21e11b0f44e5110b3d1dc06f5e32e5fd0e7f45cd
MayWil3006 <willmann.maya-it24@it.dhbw-ravensburg.de>
2025-10-14 14:33:22 +0200

    feat: add exercise number three

--- exercise-002/main.cpp
+++ exercise-002/main.cpp
@@ -4,6 +4,10 @@
 #include "CLI/CLI.hpp"
 #include "config.h"
 
+int globalVar = 1;
+
+void foo() { fmt::print("Hello from foo!\\n"); }
+
 auto main(int argc, char **argv) -> int
 {
     /**
@@ -26,9 +30,36 @@ auto main(int argc, char **argv) -> int
      * it is much more convenient than std::cout and printf
      * More info at https://fmt.dev/latest/api.html
      */
-    fmt::print("Hello, {}!\n", app.get_name());
+    fmt::print("Hello, {}!\n\n", app.get_name());
 
     /* INSERT YOUR CODE HERE */
+
+    fmt::print("Wert der Globalen Variablen globalVar = {}\n", globalVar);
+    fmt::print("Adresse der Globalen Variablen globalVar = {}\n", static_cast<const void*>(&globalVar));
+    fmt::print("Beobachtung: Globale Variablen liegen im Data-Segment\n\n");
+
+    int localVar = 2;
+    fmt::print("Wert der Lokalen Variablen localVar = {}\n", localVar);
+    fmt::print("Adresse der Lokalen Variablen localVar = {}\n", static_cast<const void*>(&localVar));
+    fmt::print("Beobachtung: Lokale Variablen liegen im Stack-Segment\n\n");
+
+    int* heapVar = new int(3);
+    fmt::print("Wert der Heap Variablen heapVar = {}\n", *heapVar);
+    fmt::print("Adresse der Heap Variablen heapVar = {}\n", static_cast<const void*>(heapVar));
+    delete heapVar;
+    fmt::print("Beobachtung: Heap Variablen liegen im Heap-Segment\n\n");
+
+    fmt::print("Adresse von foo: {}\n", fmt::ptr(&foo));
+    fmt::print("Beobachtung: Funktionen liegen im Code-Segment\n");
+
+    /*
+    Lokale Variablen werden automatisch im Stack verwaltet und man kann schnell auf sie zugreifen.
+    Dynamische Objekte im Heap müssen manuell verwaltet werden, was Auswirkungen auf Speicherverbrauch, 
+    Laufzeitverhalten und Programmstabilität hat.
+
+    Referenzen zeigen auf denselben Speicherort ohne selbst Speicher zu belegen.
+    Zeiger speichern Adressen und erlauben direkten Zugriff auf die Speicherbereiche.
+    */
 
     return 0; /* exit gracefully*/
 }
